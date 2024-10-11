625772e76b8128a66e29884db081b2e06a2c029a
DAVID MACUIXTLE VELAZQUEZ 3-W <a.23308051281192@cbtis128.edu.mx>
2024-10-10 22:31:49 -0600

    Add files via upload

---
+++ AACT11.py
@@ -0,0 +1,13 @@
+print("\nDavid Macuixtle Velazquez\n")
+#aqui se define una funcion 
+def distancia():
+    mate = ((x2 - x1)**2)+((y2 - y1)**2)
+    print("El resultado es",mate)
+
+#aqui se te pide que ingreses numeros
+x1 = int(input("escribe un numero"))
+x2 = int(input("escribe un numero"))
+y1 = int(input("escribe un numero"))
+y2 = int(input("escribe un numero"))
+
+distancia()
